4d1c3337ee6eee26e84c8a99933ff8d004f3a8e6
suyash101101 <143525373+suyash101101@users.noreply.github.com>
2024-07-05 05:15:51 +0530

    Update typeWriter.js

--- js/typeWriter.js
+++ js/typeWriter.js
@@ -1,7 +1,7 @@
 
 let i = 0;
-let text1 = "Hey! Miss Qiu.";
-let text2 = "Today is the Big Day."
+let text1 = "Hey Keyuuuuu";
+let text2 = "Alabututu"
 let speed = 100;
 
 function typeWriter(text, para){
